ca60e36448dd93c10d7e21f3540a219d9c60acf0
yboi <yaroslav@enapps.co.uk>
2022-05-06 15:00:45 +0300

    [IMP] Added correction

--- auction.py
+++ auction.py
@@ -1,31 +1,32 @@
 from replit import clear
 from art import logo
-#HINT: You can call clear() to clear the output in the console.
+
+# HINT: You can call clear() to clear the output in the console.
 print(logo)
 
+
 def auction():
-  answer = "no"
-  biders = {}
-  while answer == "no":
-    user_name = input("What's your name?\n")
-    bid = float(input("What's your bid? $\n"))
-    biders[user_name] = bid
-    answer = input("Are you the last of biders? (yes or no)\n")
-    if answer == "no":
-      clear()
-    elif answer == "yes":
-      answer = "yes"
-    else:
-      print ("Error, you must use lowercase and only 'yes' or 'no'!")
+    answer = False
+    bidders = {}
+    while not answer:
+        user_name = input("What's your name?\n")
+        bid = float(input("What's your bid? $\n"))
+        bidders[user_name] = bid
+        will_continue = input("Are you the last of bidders? (yes or no)\n")
+        if will_continue == "yes":
+            answer = True
+        elif will_continue == "no":
+            clear()
+        else:
+            print("Error, you must use lowercase and only 'yes' or 'no'!")
 
-  value = 0.00
-  for person in (0,len(biders) - 1):
-    item = list(biders.keys())[person]
-    user_value = biders.get(item)
-  if user_value > value:
-    value = user_value
-    name_usr = item
+    value = 0.00
+    for person in (0, len(bidders) - 1):
+        item = list(bidders.keys())[person]
+        user_value = bidders.get(item)
+    if user_value > value:
+        value = user_value
 
-  print(f"Winner is {name_usr} where bid is $ {value}")
+    print(f"Winner is {item} where bid is $ {value}")
 
 auction()
