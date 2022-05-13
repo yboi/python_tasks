dd48a6de92cb5b88be7a6dfcbd00eaa9af3951cc
yboi <yaroslav@enapps.co.uk>
2022-05-13 12:25:14 +0300

    [FIX] Couple fixes

--- lesson2/home_work.py
+++ lesson2/home_work.py
@@ -1,11 +1,11 @@
 # converter temperature
-degrees = int(input("Enter the temperature in degrees Celsius:\n"))
+degrees = float(input("Enter the temperature in degrees Celsius:\n"))
 fahrenheit = 1.8 * degrees + 32
 print(f"The temperature in degrees Fahrenheit is {fahrenheit}")
 
 # Problem: Pure Gold!
-pure_gold = int(input("How many K has your gold?\n"))
-if pure_gold in (22, 24):
+pure_gold = float(input("How many % has your gold?\n"))
+if pure_gold >= 91.7 and pure_gold <= 99.9:
     print("Accepted")
 else:
     print("Is not Accepted")
@@ -19,6 +19,17 @@ else:
 
 # Problem: Leap year
 year = int(input("What the year to check?\n"))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:
+            print("Not a leap year")
+    else:
+        print("Leap year")
+else:
+    print("Not a leap year")
+
 if (year % 4 == 0) and (year % 100 != 0):
     print("Leap year")
 elif (year % 400 == 0) and (year % 100 == 0):
@@ -35,13 +46,25 @@ else:
     print("Sorry")
 
 # Problem: 24K Magic
-
+pure_gold = float(input("How many % has your gold?\n"))
+if pure_gold >= 75.0 and pure_gold < 83.3:
+    print("18K")
+elif pure_gold >= 83.3 and pure_gold < 91.7:
+    print("20K")
+elif pure_gold >= 91.7 and pure_gold < 99.9:
+    print("22K")
+elif pure_gold >= 99.9:
+    print("24K")
+else:
+    print("Is lower than 75%")
 
 # Problem: Financial Transactions
 bank = input("Please enter what the bank card you have\n")
 
 if bank == "Visa" or bank == "Amex":
     print("accepted")
+else:
+    print("Bank is not supported")
 
 # Exercise: Equilateral triangle - isosceles
 print("Please input length of the triangle: ")
