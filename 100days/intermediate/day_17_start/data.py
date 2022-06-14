e9db7d0d52d0ebf69855204550ea13ea6f53fd95
yboi <yaroslav@enapps.co.uk>
2022-06-14 07:52:08 +0300

    [FIX] added OOP

---
+++ 100days/intermediate/day_17_start/data.py
@@ -0,0 +1,55 @@
+question_data = [
+    {
+        "category": "Entertainment: Film",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The word Inception came from the 2010 blockbuster hit Inception.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "Tetris is the #1 best-selling video game of all-time.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Politics", "type": "boolean", "difficulty": "easy",
+        "question": "Despite being seperated into multiple countries, The Scandinavian countries are unified by all having the same monarch.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "Gordon Freeman, the protagonist of &quot;Half-Life&quot;, is said to have once had a ponytail.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
+        "question": "The alternative rock band, They Might Be Giants, released their album &#039;Flood&#039; in 1990. ",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science & Nature", "type": "boolean", "difficulty": "easy",
+        "question": "An exothermic reaction is a chemical reaction that releases energy by radiating electricity.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "In &quot;Need for Speed: Porsche Unleashed&quot;, the player can only drive cars manufactured by Porsche.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics", "type": "boolean", "difficulty": "easy",
+        "question": "An isosceles triangle has two sides of equal length as opposed to three.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+        "question": "The game &quot;Jetpack Joyride&quot; was created by &quot;Redbrick Studios&quot;.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    }
+]
